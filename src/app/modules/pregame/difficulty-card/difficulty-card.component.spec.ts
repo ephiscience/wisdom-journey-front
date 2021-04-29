64045cc0d780f8447cbd0f955bc8a578d293118e
Rémi Piotaix <remi@piotaix.fr>
2021-04-30 00:44:01 +0200

    Added translation for difficulty selection cards

---
+++ src/app/modules/pregame/difficulty-card/difficulty-card.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DifficultyCardComponent } from 'src/app/modules/pregame/difficulty-card/difficulty-card.component';
+
+describe('DifficultyCardComponent', () => {
+  let component: DifficultyCardComponent;
+  let fixture: ComponentFixture<DifficultyCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DifficultyCardComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DifficultyCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
