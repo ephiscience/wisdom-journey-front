21789704c114bb92fb27dda1c786bbc82e7fa0a4
Rémi Piotaix <remi@piotaix.fr>
2021-04-29 13:55:48 +0200

    Some fixes
    Co-authored-by: Kappamille <camille.duquesne@cri-paris.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { TimesPipe } from 'src/app/modules/shared/times.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HomeComponent } from './home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { QuitGameConfirmationModalComponent } from 'src/app/modules/shared/quit-game-confirmation-modal/quit-game-confirmation-modal.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, GraphQLModule, HttpClientModule, NgbModule, FormsModule],
-  providers: [NgbModal],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/shared/missing-player-name-modal/missing-player-name-modal.component.ts
+++ src/app/modules/shared/missing-player-name-modal/missing-player-name-modal.component.ts
@@ -4,60 +4,52 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-missing-player-name-modal',
   template: `
-    <div class="modal-content">
-      <div class="modal-header ">
-        <h4 class="modal-title w-100 text-center"></h4>
-        <button type="button" class="close" aria-label="Close" (click)="dismiss()">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body w-100 text-center">
-        <p>Veuillez entrer un nom pour chaque joueur</p>
-      </div>
-      <div class="modal-footer justify-content-center">
-        <button type="button" class="btn-ok" (click)="dismiss()">Ok</button>
-      </div>
+    <div class="modal-header">
+      <h4 class="modal-title w-100 text-center"></h4>
+      <button type="button" class="close" aria-label="Close" (click)="dismiss()">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body w-100 text-center">
+      <p>Veuillez entrer un nom pour chaque joueur</p>
+    </div>
+    <div class="modal-footer justify-content-center">
+      <button type="button" class="btn-ok" (click)="dismiss()">Ok</button>
     </div>
   `,
   styles: [
     `
-      ::ng-deep .modal-content {
-        //opacity: 1;
-        z-index: 100;
-        width: 479px;
-        height: 301px;
-        border-radius: 56px;
-
-        box-shadow: 0px 0px 10px #000000;
-
-        background-color: #ffffff;
-      }
       .modal-header {
-        border-bottom: 0px;
+        border-bottom: 0;
       }
+
       .modal-title {
         text-align: center;
         font: normal normal normal 60px/71px Chela One;
         color: #000000;
       }
+
       .modal-body {
         font: normal normal normal 33px/39px Roboto;
         color: #000000;
-        border-bottom: 0px;
+        border-bottom: 0;
       }
+
       .modal-footer {
-        border-top: 0px;
+        border-top: 0;
       }
+
       .btn-ok {
         width: 141px;
         height: 66px;
-        background: #b2b2b2 0% 0% no-repeat padding-box;
-        box-shadow: 0px 3px 6px #00000029;
+        background: #b2b2b2 0 0 no-repeat padding-box;
+        box-shadow: 0 3px 6px #00000029;
         border: 2px solid #707070;
         border-radius: 33px;
         text-align: center;
         font: normal normal normal 45px/53px Chela One;
       }
+
       .close {
         font-size: 40px;
       }
